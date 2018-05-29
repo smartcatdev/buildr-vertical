7515ef671a3e9c1925d6917012647a8d8d08f4ac
Zack Mitchell <zackcmitchell@gmail.com>
2018-05-28 22:36:29 -0400

    Initial Work on Vertical Navbar

---
+++ inc/functions-enqueue.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Enqueue scripts and styles.
+ */
+function buildr_vertical_scripts() {
+    
+    $parent_style = 'buildr-style';
+
+    // Styles
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'buildr-vertical', 
+        get_stylesheet_directory_uri() . '/assets/css/buildr-vertical.css', 
+        array( $parent_style, 'buildr-util', 'buildr' ), 
+        BUILDR_VERSION 
+    );
+    
+    // Scripts
+    
+    
+}
+add_action( 'wp_enqueue_scripts', 'buildr_vertical_scripts' );
